1a55f8d2e54ac62bbe4990469b427a90e54c4071
Ian Pineda Nieto <188219464+ianpn@users.noreply.github.com>
2025-09-16 00:12:29 -0500

    Major change: turned the project into a statically buildable offline project.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,12 +14,12 @@ export default function NavigationMenu() {
     return (
     <nav className="block sm:flex justify-between p-4 text-center sm:text-start">
         <ul className="block">
-            <li className="block lg:inline-block m-4"><Link href="/administracion/proyectos" className={pathname == "/administracion/proyectos" ? "underline font-extrabold" : "underline"}>Administrar Proyectos</Link></li>
-            <li className="block lg:inline-block m-4"><Link href="/administracion/administradores" className={pathname == "/administracion/administradores" ? "underline font-extrabold" : "underline"}>Administrar Administradores de Proyectos</Link></li>
-            <li className="block lg:inline-block m-4"><Link href="/administracion/desarrolladores" className={pathname == "/administracion/desarrolladores" ? "underline font-extrabold" : "underline"}>Administrar Desarrolladores</Link></li>
+            <li className="block lg:inline-block m-4"><Link href="/proyectos.html" className={pathname == "/proyectos.html" ? "underline font-extrabold" : "underline"}>Administrar Proyectos</Link></li>
+            <li className="block lg:inline-block m-4"><Link href="/administradores.html" className={pathname == "/administradores.html" ? "underline font-extrabold" : "underline"}>Administrar Administradores de Proyectos</Link></li>
+            <li className="block lg:inline-block m-4"><Link href="/desarrolladores.html" className={pathname == "/desarrolladores.html" ? "underline font-extrabold" : "underline"}>Administrar Desarrolladores</Link></li>
         </ul>
         <ul className="block">
-            <li className="block lg:inline-block m-4"><Link href="/administracion/perfil" className={pathname == "/administracion/perfil" ? "underline font-extrabold" : "underline"}>Perfil</Link></li>
+            <li className="block lg:inline-block m-4"><Link href="/perfil.html" className={pathname == "/perfil.html" ? "underline font-extrabold" : "underline"}>Perfil</Link></li>
             <li className="block lg:inline-block m-4"><a href="#" className="underline" onClick={handleLogoutClick}>Cerrar Sesión</a></li>
         </ul>
     </nav>
